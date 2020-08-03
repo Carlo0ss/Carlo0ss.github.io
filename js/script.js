3f413cac4f0d7167eb97e4a8b03989c05a236b98
Carlo0ss <69120265+Carlo0ss@users.noreply.github.com>
2020-08-03 02:15:01 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var ShroomParClic;
 var shroom=0;
 var shroomSave=parseFloat(localStorage.nbshrooms);
 if(parseInt(localStorage.nbshrooms)>0){
-	shroom=shroom+shroomSave;
+	shroom=shroom+999999999;
 }
 
 
